69c4ff30f73a0dbce921b51417efd146d449d9c9
muassif <32828975+muassif@users.noreply.github.com>
2021-08-11 17:29:14 +0400

    Deleting to swap chapter 13 and 14

--- Chapter13/netconf/show_all_interfaces.py
+++
@@ -1,7 +0,0 @@
-from ncclient import manager
-with manager.connect(host='172.16.2.50', username='root',
-                     password='rootroot', hostkey_verify=False) as m:
-    result = m.get_config("running",
-                          filter=('subtree',
-                                  '<interfaces xmlns="http://openconfig.net/yang/interfaces"/>'))
-    print (result)
