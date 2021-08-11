c3359bac63fae156a21c0d5c3d463678e2f2f221
muassif <32828975+muassif@users.noreply.github.com>
2021-08-11 17:29:36 +0400

    Deleting to swap chapter 13 and 14

--- Chapter13/nsp/location_services2.py
+++
@@ -1,6 +0,0 @@
-import requests
-payload = {}
-headers = {}
-url = "https://10.52.90.101/rest-gateway/rest/api/v1/location/services/endpoints?endPoint=/v1/auth/token"
-resp = requests.request("GET", url, headers=headers, data=payload, verify=False)
-print(resp.text)
