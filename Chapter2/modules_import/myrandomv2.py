74168950858ff8c46aaa5cfc067b34b2c4c05abd
muassif <32828975+muassif@users.noreply.github.com>
2021-08-12 13:44:25 +0400

    updating myrandom.py versions

--- Chapter2/modules_import/myrandomv2.py
+++ Chapter2/modules_import/myrandomv2.py
@@ -13,3 +13,7 @@ def random_1d():
 def random_2d():
    """This function get a random number between 10 and 99"""
    return random.randint(10,99)
+
+def get_random(lower, upper):
+   """This function get a random number between lower and upper"""
+   return random.randint(lower,upper)
