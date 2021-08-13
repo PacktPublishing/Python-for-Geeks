9d7143dfdf1e2f5852178f23b2539b7b2e34b3e6
muassif <32828975+muassif@users.noreply.github.com>
2021-08-13 14:52:59 +0400

    fixed html file names

--- Chapter10/webapps/app3.py
+++ Chapter10/webapps/app3.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    hello_url = url_for ('static', filename='app3.html')
+    hello_url = url_for ('static', filename='app3_s.html')
     return redirect(hello_url)
 
 @app.route('/greeting')
 def greeting():
     msg = "Hello from Python"
-    return render_template('app3.html', greeting=msg)
+    return render_template('app3_d.html', greeting=msg)
 
 if __name__ == '__main__':
     app.run()
