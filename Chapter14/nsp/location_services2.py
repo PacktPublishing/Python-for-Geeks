3c1e9afa80a965d86dd64ff9da3f515ea264360d
muassif <32828975+muassif@users.noreply.github.com>
2021-08-21 17:15:28 +0400

    address update

--- Chapter14/nsp/location_services2.py
+++ Chapter14/nsp/location_services2.py
@@ -1,6 +1,6 @@
 import requests
 payload = {}
 headers = {}
-url = "https://10.52.90.101/rest-gateway/rest/api/v1/location/services/endpoints?endPoint=/v1/auth/token"
+url = "https://10.52.90.10/rest-gateway/rest/api/v1/location/services/endpoints?endPoint=/v1/auth/token"
 resp = requests.request("GET", url, headers=headers, data=payload, verify=False)
 print(resp.text)
