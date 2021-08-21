88fc955700e7ae41dedd20c12d32c6fa10cda44e
muassif <32828975+muassif@users.noreply.github.com>
2021-08-21 17:13:33 +0400

    Update show_all_interfaces.py

--- Chapter14/netconf/show_all_interfaces.py
+++ Chapter14/netconf/show_all_interfaces.py
@@ -1,6 +1,6 @@
 from ncclient import manager
-with manager.connect(host='172.16.2.50', username='root',
-                     password='rootroot', hostkey_verify=False) as m:
+with manager.connect(host='172.16.2.34', username='root',
+                     password='xxxx', hostkey_verify=False) as m:
     result = m.get_config("running",
                           filter=('subtree',
                                   '<interfaces xmlns="http://openconfig.net/yang/interfaces"/>'))
