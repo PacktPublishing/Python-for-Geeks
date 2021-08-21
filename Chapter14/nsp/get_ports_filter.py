29c4fdbd79dbcd3bcf539fd231c835ba2c1ede88
muassif <32828975+muassif@users.noreply.github.com>
2021-08-21 17:17:38 +0400

    address update

--- Chapter14/nsp/get_ports_filter.py
+++ Chapter14/nsp/get_ports_filter.py
@@ -6,6 +6,6 @@ headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {}'.format(token)
 }
-url = "https://10.52.90.101:8544/NetworkSupervision/rest/api/v1/ports?filter=(name='Port 1/1/1' and neId='10.172.172.172')"
+url = "https://10.52.90.10:8544/NetworkSupervision/rest/api/v1/ports?filter=(name='Port 1/1/1' and neId='10.172.172.172')"
 resp = requests.request("GET", url, headers=headers, data=payload, verify=False)
 print(resp.text)
