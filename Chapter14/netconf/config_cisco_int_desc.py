7cd9ae27c3250f30b5b1d4eb99b9f081e5f3559c
muassif <32828975+muassif@users.noreply.github.com>
2021-08-21 17:14:21 +0400

    updating IP and passwd

--- Chapter14/netconf/config_cisco_int_desc.py
+++ Chapter14/netconf/config_cisco_int_desc.py
@@ -5,8 +5,8 @@ nc_payload = nc_template.\
                     format(int_name='Loopback0',
                           int_desc="Configured by NETCONF")
 
-with manager.connect(host='172.16.2.50', username='root',
-                     password='rootroot', hostkey_verify=False) as nc_conn:
+with manager.connect(host='172.16.2.34', username='root',
+                     password='xxxx', hostkey_verify=False) as nc_conn:
     netconf_reply = nc_conn.edit_config(nc_payload, target="candidate")
     print(netconf_reply)
     reply = nc_conn.commit()
