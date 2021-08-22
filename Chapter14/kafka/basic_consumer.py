de5c00ce151145276cdadbb72f83f9f80709a088
muassif <32828975+muassif@users.noreply.github.com>
2021-08-22 12:36:16 +0400

    added try, except and finally

--- Chapter14/kafka/basic_consumer.py
+++ Chapter14/kafka/basic_consumer.py
@@ -2,13 +2,20 @@ from kafka import KafkaConsumer
 import json, sys
 host_id = '10.52.90.10:9092'
 topicid = 'ns-eg-ff15a252-f927-48c7-a98f-2965ab6c187d'
-consumer = KafkaConsumer(topicid,
+consumer = KafkaConsumer(topic_id,
                          group_id='120',
-                         bootstrap_servers=['10.52.90.10:9092'], value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                         bootstrap_servers=[host_id], value_deserializer=lambda m: json.loads(m.decode('ascii')),
                          api_version=(0, 10, 1))
+try:
+    for message in consumer:
+        if message is None:
+            continue
+        else:
+            print(json.dumps(message.value, indent=4, sort_keys=True))
 
-for message in consumer:
-    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                         message.offset, message.key,
-                                         message.value))
+except KeyboardInterrupt:
+    sys.stderr.write('++++++ Aborted by user ++++++++\n')
+
+finally:
+    consumer.close()
     
